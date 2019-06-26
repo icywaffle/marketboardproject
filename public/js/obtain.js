c02a1b2f23a1fcfd317a1ba79d087f6c1d0a1978
Jacob Nguyen <synkre@gmail.com>
2019-06-25 22:51:38 -0700

    Updated Front-End Pages
    Added more styling and positioning, to get a cleaner look.

--- public/js/obtain.js
+++ public/js/obtain.js
@@ -9,9 +9,7 @@ function removeemptymaterials() {
 function getitemicon() {
     var srcvalue = '0' + $(".itemicon").attr("src");
     var folderidentifier = srcvalue.substring(1, 3);
-    var folder = '0' + folderidentifier + '000/'
-    var path = '/public/img/icon/' + folder + srcvalue + '.png'
+    var folder = '0' + folderidentifier + '000/';
+    var path = '/public/img/icon/' + folder + srcvalue + '.png';
     $(".itemicon").attr("src", path);
-    document.write(path);
-
 }
